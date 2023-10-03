afdd622ec28670ebea7332c695213d41a6583149
EmaMaker <emamaker0@gmail.com>
2023-10-03 18:43:29 +0200

    refactor message system between main and update threads
    restores blockpicking with new multithread system

--- src/controls.cpp
+++ src/controls.cpp
@@ -1,4 +1,8 @@
 #include "controls.hpp"
+
+#include "camera.hpp"
+#include "chunkmanager.hpp"
+#include "globals.hpp"
 #include "renderer.hpp"
 
 namespace controls{
@@ -12,23 +16,39 @@ namespace controls{
     void update(GLFWwindow* window){
 	float current_time = glfwGetTime();
 
-	// Reset blockpicking timeout has passed
+	/* BlockPicking */
+	// Reset blockpicking if enough time has passed
 	if(current_time - lastBlockPick > BLOCKPICK_TIMEOUT) blockpick = false;
 	// Reset blockpicking if both mouse buttons are released
 	if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_1) == GLFW_RELEASE && glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_2) == GLFW_RELEASE) blockpick = false;
+	// Process block picking if a mouse button is pressed
+	if((glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_1) == GLFW_PRESS ||
+		glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_2 == GLFW_PRESS)) && !blockpick){
 
-	if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_2) == GLFW_PRESS && !blockpick){
-	    //chunkmanager::blockpick(false);
-	    blockpick=true;
-	    lastBlockPick=glfwGetTime();
+	    // Start timeout for next block pick action
+	    blockpick =  true;
+	    lastBlockPick = current_time;
+
+	    // Construct the message to send to chunkmanager
+
+	    // WorldUpdateMsg is allocated on the stack
+	    // unlike ChunkMeshData, the fields of WorldUpdateMsg are few and light, so there's no
+	    // problem in passing them by value each time.
+	    // It also has the advantage of having less memory to manage, since I'm not allocating
+	    // anything on the heap
+
+	    WorldUpdateMsg msg{};
+	    msg.cameraPos = theCamera.getPos();
+	    msg.cameraFront = theCamera.getFront();
+	    msg.time = current_time;
+	    msg.msg_type = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_1) == GLFW_PRESS ?
+	    WorldUpdateMsgType::BLOCKPICK_PLACE : WorldUpdateMsgType::BLOCKPICK_BREAK;
+
+	    // Send to chunk manager
+	    chunkmanager::getWorldUpdateQueue().push(msg);
 	}
 
-	if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_1) == GLFW_PRESS && !blockpick){
-	    //chunkmanager::blockpick(true);
-	    blockpick=true;
-	    lastBlockPick=glfwGetTime();
-	}
-
+	/* SCREENSHOTS */
 	if(glfwGetKey(window, GLFW_KEY_F2) == GLFW_PRESS) renderer::saveScreenshot();
 	if(glfwGetKey(window, GLFW_KEY_F3) == GLFW_PRESS) renderer::saveScreenshot(true);
 	if(glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS) {
